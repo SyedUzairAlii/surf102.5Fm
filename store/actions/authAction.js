efc75d25f4280802c448a99a363b4760f86f69d9
SyedUzairAlii <maazuzair3322@gmail.com>
2020-01-04 21:11:39 +0500

    action

--- store/actions/authAction.js
+++ store/actions/authAction.js
@@ -5,10 +5,8 @@ import { StackActions, NavigationActions } from 'react-navigation';
 export function setDataReducer(state, data) {
     return dispatch => {
         return new Promise(function (resolve, reject) {
-
             dispatch({ type: ActionTypes[state], payload: data })
             resolve(true)
-
         })
     }
 }
