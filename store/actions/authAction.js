3353a74990a1eafb2e74eef3e3182426ef3c1947
SyedUzairAlii <maazuzair3322@gmail.com>
2020-01-13 02:12:25 +0500

    spaces

--- store/actions/authAction.js
+++ store/actions/authAction.js
@@ -1,7 +1,6 @@
 import ActionTypes from '../Constant/Constant'
 import { StackActions, NavigationActions } from 'react-navigation';
 
-
 export function setDataReducer(state, data) {
     return dispatch => {
         return new Promise(function (resolve, reject) {
